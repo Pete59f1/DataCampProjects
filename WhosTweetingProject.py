2435a277e926a2b1ba7e2dd2162ebc1c86f7c43a
starup <pe7913@hotmail.com>
2020-03-15 11:20:58 +0100

    Added dataset for WhosTweetingProject, and made few changes

--- WhosTweetingProject.py
+++ WhosTweetingProject.py
@@ -7,15 +7,15 @@ from sklearn import metrics
 import pandas as pd
 
 # importing data & splitting into training and test data
-data = pd.read_csv("datasets/tweets.csv") # Don't have the dataset
+data = pd.read_csv("WhosTweeting.csv")
 tweet_df = pd.DataFrame(data)
 y = tweet_df.author
 X_train, X_test, y_train, y_test = train_test_split(tweet_df.status, y, random_state=53, test_size=.33)
 
 count_vectorizer = CountVectorizer(min_df=0.05, max_df=0.9)
-count_train = count_vectorizer.fit_transform() # What to do here?
-count_test = count_vectorizer.transform() # What to do here?
+count_train = count_vectorizer.fit_transform()
+count_test = count_vectorizer.transform()
 
 tfidf_vectorizer = TfidfVectorizer(min_df=0.05, max_df=0.9)
-tfidf_train = tfidf_vectorizer.fit_transform() # What to do here?
-tfidf_test = tfidf_vectorizer.transform() # What to do here?
+tfidf_train = tfidf_vectorizer.fit_transform()
+tfidf_test = tfidf_vectorizer.transform()
