ed0b983745436d9925137a57fc6a742576e01308
starup <pe7913@hotmail.com>
2020-03-15 13:49:26 +0100

    Finished the who's tweeting project, and also added the helper functions and the notebook file

--- WhosTweetingProject.py
+++ WhosTweetingProject.py
@@ -6,6 +6,9 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import LinearSVC
 from sklearn import metrics
 import pandas as pd
+from pprint import pprint
+from WhosTweetingHelper_functions import plot_confusion_matrix
+from WhosTweetingHelper_functions import plot_and_return_top_features
 
 # Task 2
 # importing data & splitting into training and test data
@@ -49,3 +52,39 @@ print('NaiveBayes Tfidf Score: ', tfidf_nb_score)
 print('NaiveBayes Count Score: ', count_nb_score)
 
 # Task 5
+tfidf_nb_cm = metrics.confusion_matrix(y_test, tfidf_nb_pred, labels=['Donald J. Trump', 'Justin Trudeau'])
+count_nb_cm = metrics.confusion_matrix(y_test, count_nb_pred, labels=['Donald J. Trump', 'Justin Trudeau'])
+
+plot_confusion_matrix(tfidf_nb_cm, classes=['Donald J. Trump', 'Justin Trudeau'], title="TF-IDF NB Confusion Matrix")
+plot_confusion_matrix(count_nb_cm, classes=['Donald J. Trump', 'Justin Trudeau'], title="Count NB Confusion Matrix", figure=1)
+
+# Task 6
+# Creating and training model
+tfidf_svc = LinearSVC()
+tfidf_svc.fit(tfidf_train, y_train)
+
+# Predicting and getting score
+tfidf_svc_pred = tfidf_svc.predict(tfidf_test)
+tfidf_svc_score = metrics.accuracy_score(tfidf_svc_pred, y_test)
+
+print("LinearSVC Score:   %0.3f" % tfidf_svc_score)
+
+svc_cm = metrics.confusion_matrix(y_test, tfidf_svc_pred, labels=['Donald J. Trump', 'Justin Trudeau'])
+plot_confusion_matrix(svc_cm, classes=['Donald J. Trump', 'Justin Trudeau'], title="TF-IDF LinearSVC Confusion Matrix")
+
+# Task 7
+top_features = plot_and_return_top_features(tfidf_svc, tfidf_vectorizer)
+pprint(top_features)
+
+# Task 8
+trump_tweet = "Build the wall"
+trudeau_tweet = "Now its Canada"
+
+trump_tweet_vectorized = tfidf_vectorizer.transform([trump_tweet])
+trudeau_tweet_vectorized = tfidf_vectorizer.transform([trudeau_tweet])
+
+trump_tweet_pred = tfidf_svc.predict(trump_tweet_vectorized)
+trudeau_tweet_pred = tfidf_svc.predict(trudeau_tweet_vectorized)
+
+print("Predicted Trump tweet", trump_tweet_pred)
+print("Predicted Trudeau tweet", trudeau_tweet_pred)
